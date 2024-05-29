5393ed7755ba87e081bc96eedb5414aab034aa01
mandrindra12 <mandrindraantonnio@gmail.com>
2024-05-29 23:00:17 +0300

    update models according to data(not achieved yet)

--- models/models.go
+++ models/models.go
@@ -2,27 +2,42 @@ package models
 
 import (
 	"github.com/dgrijalva/jwt-go"
-	_ "golang.org/x/text/date"
+	"gorm.io/gorm"
+	"time"
 )
 
 type Credentials struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
-
 type Claims struct {
 	Username string
+	Type     string
 	jwt.StandardClaims
 }
 
-type Event struct {
-	Title       string `json:"title"`
-	ImagePath   string `json:"path"`
-	Description string `json:"description"`
-	// _ StartDate   date   `json:"start_date"`
-	// _ EndDate     date   `json:"end_date"`
+type User struct {
+	gorm.Model
+	Username string `gorm:"varchar(200)"`
+	Password string `gorm:"varchar(200)"`
 }
 
-type CustomersMail struct {
-	email string
+type Event struct {
+	gorm.Model
+	Title       string `gorm:"varchar(100)"`
+	Description string `gorm:"varchar(500)"`
+	EventType   string `gorm:"varchar(100)"`
+	StartDate   time.Time
+	EndDate     time.Time
+	ImagePath   string `gorm:"varchar(200)"`
+}
+
+type Mail struct {
+	gorm.Model
+	EmailAddress string `json:"email"`
+}
+
+type Content struct {
+	gorm.Model
+	Path string
 }
